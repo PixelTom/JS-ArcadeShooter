1305a357c3ac42ad4cd00b364ccdef5eeb8a5062
Thomas Gattenhof <thomas@3rdsense.com>
2014-04-04 17:57:01 +1100

    Prototype 0.1.0
    Working basic prototype. Hold mouse down to move player.

--- com/arcadeShooter.js
+++ com/arcadeShooter.js
@@ -4,11 +4,18 @@ var bullets; var enemies;
 var gameOver = false;
 var scoreText;
 var player;
+var playerSpeed = 5;
+var fireRate; var fireRateMax = 30;
+var tTimer; var cEvent;
+var emitter;
 //******************************************************************************************************
 // 
 //******************************************************************************************************
 function preload() {
+	game.load.image('bullet1', 'lib/laserRed01.png')
+	game.load.image('bone', 'lib/bone.png')
     game.load.spritesheet('player', 'lib/dude.png', 66, 82);
+    game.load.spritesheet('bat', 'lib/bat.png', 88, 47);
 	//game.load.spritesheet('name', 'lib/name.png', width, height);
 }
 //******************************************************************************************************
@@ -16,29 +23,36 @@ function preload() {
 //******************************************************************************************************
 function create() {
 	// Initiate Physics
-	//game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	bullets = game.add.group();
 	enemies = game.add.group();
+	bullets = game.add.group();
 
-	bullets.enableBody = true;
 	enemies.enableBody = true;
+	bullets.enableBody = true;
 
 	buildPlayer();
 
 	scoreText = game.add.text(16, 16, 'Distance: 0', { fontSize: '32px', fill: '#000' });
 
-	/*emitter = game.add.emitter(0, 0, 200);
-    emitter.makeParticles('star');
-    emitter.gravity = 0;*/
+	fireRate = 0;
+
+	tTimer = game.time.create(false);
+	cEvent = tTimer.loop(2000, spawnBaddie, this);
+	tTimer.start();
+
+	emitter = game.add.emitter(0, 0, 200);
+    emitter.makeParticles('bone');
+    emitter.gravity = 0;
 }
 //******************************************************************************************************
 // 
 //******************************************************************************************************
 function buildPlayer(){
 	// Legacy code, to be updated
-	player = game.add.sprite(100, 600, 'player');
-	game.physics.arcade.enable(player);
+	player = game.add.sprite(100, 650, 'player');
+	player.anchor.setTo(0.5,0.5)
+	//game.physics.arcade.enable(player);
 	player.animations.add('run',[0,1],5,true);
 	player.animations.play('run');
 }
@@ -47,12 +61,60 @@ function buildPlayer(){
 //******************************************************************************************************
 function update() {
 	if(gameOver)return;
+	if(game.input.mousePointer.isDown){
+		if(player.x + 5 < game.input.mousePointer.x)player.x += playerSpeed;
+		if(player.x - 5 > game.input.mousePointer.x)player.x -= playerSpeed;
+		fireRate--;
+		if(fireRate <= 0)fire();
+	}else{
+		fireRate = 0;
+	}
+	bullets.forEach(checkBulletStatus,this,true);
+	game.physics.arcade.overlap(bullets,enemies,shotBaddie);
+}
+//******************************************************************************************************
+// 
+//******************************************************************************************************
+function spawnBaddie(){
+	// currently bat only
+	baddie = enemies.create(rn(50,400),-50,'bat');
+	baddie.anchor.setTo(0.5,0.5);
+	baddie.animations.add('normal',[0,1],5,true);
+	hitAnimation = baddie.animations.add('hit',[2,0,2],10,false);
+	hitAnimation.onComplete.add(afterHit,this);
+	baddie.animations.play('normal');
+	baddie.body.velocity.y = 100;
+	baddie.health = 3;
+}
+//******************************************************************************************************
+// 
+//******************************************************************************************************
+function fire(){
+	bullet = bullets.create(player.x,player.y,'bullet1');
+	bullet.anchor.setTo(0.5,0.5);
+	//bullet.body.gravity = 0;
+	bullet.body.velocity.y = -500;
+	fireRate = fireRateMax
+}
+function shotBaddie(bullet, baddie){
+	bullet.kill();
+	baddie.animations.play('hit');
+	baddie.health--;
+	if(baddie.health <= 0){
+		baddie.destroy();
+		emitter.x = baddie.x;
+		emitter.y = baddie.y;
+		emitter.start(true, 500, null, rn(3,5));
+	}
+}
+function afterHit(e){
+	e.animations.play('normal');
 }
 //******************************************************************************************************
 // 
 //******************************************************************************************************
 function checkBulletStatus(e){
-	//if(bullets off screen) e.kill();
+	if(e.y - 20 < 0) e.kill();
 }
 //******************************************************************************************************
 // 
@@ -95,3 +157,6 @@ function rn(min, max) {
 	return Math.round(Math.random() * (max - min)) + min;
 }
 //******************************************************************************************************
+function fleeceItOut(){
+	trace("fleece");
+}
